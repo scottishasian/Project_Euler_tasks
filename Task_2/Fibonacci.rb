38f570b10264c99643d4bd14c519503666a7c567
Kynan <k.song@blueyonder.co.uk>
2017-10-05 10:19:55 +0100

    Completed Fibonacci Sequence Task.

--- Task_2/Fibonacci.rb
+++ Task_2/Fibonacci.rb
@@ -1,22 +1,50 @@
+#
+# def iterate(number)
+#
+#   evens = []
+#
+#
+#   for i in 1..(Fib(number))
+#     evens.push(i)
+#   end
+#   puts evens
+# end
 
-def iterate(number)
 
-  evens = []
-
-
-  for i in 1..Fib(number)
-    evens.push(i)
-  end
-  puts evens
-end
-
-
-def Fib(number)
-  return number if (0..1).include? number
-    (Fib(number-1) + Fib(number-2))
-end
+# def Fib(number)
 
 
 
+  # return number if (1..2).include? number
+  #   (Fib(number-1) + Fib(number-2))
 
-puts iterate(10)
+# end
+#
+#
+#
+# puts Fib()
+# puts iterate(10)
+
+
+
+
+
+  evens = [1,2]
+  evens2 = []
+
+    for i in evens
+      if evens[-1] + evens[-2] < 4000000
+        evens << evens[-1] + evens[-2]
+      end
+
+      for i in evens
+        if i % 2 == 0
+          evens2 << i
+        end
+      end
+
+
+    end
+
+  # puts evens
+  puts evens2.uniq.sum
--- Task_3/Prime_factors.rb
+++ Task_3/Prime_factors.rb
@@ -3,14 +3,14 @@ def primefactors(number)
   primes = []
   final = []
 
-  for i in 1...number
+
+  for i in (1...number).reverse_each
     if number % i == 0
       primes << i
     end
   sorted = primes.uniq
   end
   return sorted
-
 
 end
 
